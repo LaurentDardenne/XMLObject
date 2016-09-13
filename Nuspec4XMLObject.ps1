4b0b16e40c2f6048655d1cebc4056365c645d45d
LaurentDardenne <LaDardenne@Orange.fr>
2016-09-13 14:12:39 +0200

    Profile of project. Script to create the nuspec file

---
+++ Nuspec4XMLObject.ps1
@@ -0,0 +1,21 @@
+﻿
+nuspec 'XMLObject' '1.0.0.0' {
+   properties @{
+        Authors='Dardenne Laurent'
+        Description="Management of class linked to an XSD schema."
+        title='XMLObject'
+        summary='Transforms an XML file into a class C# and vice versa.'
+        copyright='Copyleft'
+        language='en-US'
+        licenseUrl='https://creativecommons.org/licenses/by-nc-sa/4.0/'
+        projectUrl='https://github.com/LaurentDardenne/XMLObject'
+        iconUrl='https://github.com/LaurentDardenne/XMLObject/blob/master/icon/XMLObjects.png'
+        releaseNotes=''
+        tags=$null
+   }
+   
+   files {
+    file -src "G:\PS\XMLObject\XMLObject.psd1"
+    file -src "G:\PS\XMLObject\XMLObject.psm1"
+   }
+}|Save-Nuspec -FileName "$XMLObjectDelivery\XMLObject.nuspec" -nobom
---
+++ XMLObject_ProjectProfile.ps1
@@ -0,0 +1,44 @@
+﻿Param (
+ # Specific to the development computer
+ [string] $VcsPathRepository=''
+) 
+
+if (Test-Path env:APPVEYOR_BUILD_FOLDER)
+{
+  $VcsPathRepository=$env:APPVEYOR_BUILD_FOLDER
+}
+
+if (!(Test-Path $VcsPathRepository))
+{
+  Throw 'Configuration error, the variable $VcsPathRepository should be configured.'
+}
+
+#Variable commune à tous les postes
+#todo ${env:Name with space}
+if ( $null -eq [System.Environment]::GetEnvironmentVariable("ProfileXMLObject","User"))
+{ 
+ [Environment]::SetEnvironmentVariable("ProfileXMLObject",$VcsPathRepository, "User")
+  #refresh the environment Provider
+ $env:ProfileXMLObject=$VcsPathRepository 
+}
+
+ # Variable spécifiques au poste de développement
+$XMLObjectDelivery= "${env:temp}\Delivery\XMLObject"   
+$XMLObjectLogs= "${env:temp}\Logs\XMLObject" 
+
+ # Variable communes à tous les postes, leurs contenu est spécifique au poste de développement
+$XMLObjectBin= "$VcsPathRepository\Bin"
+$XMLObjectHelp= "$VcsPathRepository\Documentation\Helps"
+$XMLObjectSetup= "$VcsPathRepository\Setup"
+$XMLObjectVcs= "$VcsPathRepository"
+$XMLObjectTests= "$VcsPathRepository\Tests"
+$XMLObjectTools= "$VcsPathRepository\Tools"
+$XMLObjectUrl= 'https://github.com/LaurentDardenne/XMLObject.git'
+
+ #PSDrive sur le répertoire du projet 
+$null=New-PsDrive -Scope Global -Name XMLObject -PSProvider FileSystem -Root $XMLObjectVcs 
+
+Write-Host "Settings of the variables of XMLObject project." -Fore Green
+
+rv VcsPathRepository
+
